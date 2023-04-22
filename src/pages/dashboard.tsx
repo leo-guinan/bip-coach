d9a8a19f762661ec57cd7ecf7298adc4d24714e7
leo-guinan <leo@copy.ai>
2023-04-22 16:29:46 -0400

    cleaning up sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -90,11 +90,7 @@ const DashboardPage = () => {
                       {/* Sidebar component, swap this element with another sidebar if you like */}
                       <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6 pb-2">
                         <div className="flex h-16 shrink-0 items-center">
-                          <img
-                            className="h-8 w-auto"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=white"
-                            alt="Your Company"
-                          />
+                          <img className="h-8 w-auto" src="/logo.png" alt="Choose Your Algorithm" />
                         </div>
                         <nav className="flex flex-1 flex-col">
                           <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -129,31 +125,31 @@ const DashboardPage = () => {
                                 ))}
                               </ul>
                             </li>
-                            <li>
-                              <div className="text-xs font-semibold leading-6 text-indigo-200">
-                                Referrals
-                              </div>
-                              <ul role="list" className="-mx-2 mt-2 space-y-1">
-                                {referralLinks.map((team) => (
-                                  <li key={team.name}>
-                                    <a
-                                      href={team.href}
-                                      className={classNames(
-                                        team.current
-                                          ? "bg-indigo-700 text-white"
-                                          : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                                        "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                      )}
-                                    >
-                                      <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                        {team.initial}
-                                      </span>
-                                      <span className="truncate">{team.name}</span>
-                                    </a>
-                                  </li>
-                                ))}
-                              </ul>
-                            </li>
+                            {/*<li>*/}
+                            {/*  <div className="text-xs font-semibold leading-6 text-indigo-200">*/}
+                            {/*    Referrals*/}
+                            {/*  </div>*/}
+                            {/*  <ul role="list" className="-mx-2 mt-2 space-y-1">*/}
+                            {/*    {referralLinks.map((team) => (*/}
+                            {/*      <li key={team.name}>*/}
+                            {/*        <a*/}
+                            {/*          href={team.href}*/}
+                            {/*          className={classNames(*/}
+                            {/*            team.current*/}
+                            {/*              ? "bg-indigo-700 text-white"*/}
+                            {/*              : "text-indigo-200 hover:text-white hover:bg-indigo-700",*/}
+                            {/*            "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"*/}
+                            {/*          )}*/}
+                            {/*        >*/}
+                            {/*          <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">*/}
+                            {/*            {team.initial}*/}
+                            {/*          </span>*/}
+                            {/*          <span className="truncate">{team.name}</span>*/}
+                            {/*        </a>*/}
+                            {/*      </li>*/}
+                            {/*    ))}*/}
+                            {/*  </ul>*/}
+                            {/*</li>*/}
                           </ul>
                         </nav>
                       </div>
@@ -166,13 +162,9 @@ const DashboardPage = () => {
             {/* Static sidebar for desktop */}
             <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
               {/* Sidebar component, swap this element with another sidebar if you like */}
-              <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-indigo-600 px-6">
-                <div className="flex h-16 shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=white"
-                    alt="Your Company"
-                  />
+              <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-800 px-6">
+                <div className="flex h-32 shrink-0 items-center mx-auto">
+                  <img className="h-16 w-auto" src="./logo.png" alt="Choose Your Algorithm" />
                 </div>
                 <nav className="flex flex-1 flex-col">
                   <ul role="list" className="flex flex-1 flex-col gap-y-7">
@@ -187,8 +179,8 @@ const DashboardPage = () => {
                               }}
                               className={classNames(
                                 item.current
-                                  ? "bg-indigo-700 text-white"
-                                  : "text-indigo-200 hover:text-white hover:bg-indigo-700",
+                                  ? "bg-gray-800 text-white"
+                                  : "text-indigo-200 hover:text-white hover:bg-gray-800",
                                 "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                               )}
                             >
@@ -207,45 +199,45 @@ const DashboardPage = () => {
                         ))}
                       </ul>
                     </li>
-                    <li>
-                      <div className="text-xs font-semibold leading-6 text-indigo-200">
-                        Referrals
-                      </div>
-                      <ul role="list" className="-mx-2 mt-2 space-y-1">
-                        {referralLinks.map((team) => (
-                          <li key={team.name}>
-                            <a
-                              href={team.href}
-                              className={classNames(
-                                team.current
-                                  ? "bg-indigo-700 text-white"
-                                  : "text-indigo-200 hover:text-white hover:bg-indigo-700",
-                                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                              )}
-                            >
-                              <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">
-                                {team.initial}
-                              </span>
-                              <span className="truncate">{team.name}</span>
-                            </a>
-                          </li>
-                        ))}
-                      </ul>
-                    </li>
-                    <li className="-mx-6 mt-auto">
-                      <a
-                        href="#"
-                        className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700"
-                      >
-                        <img
-                          className="h-8 w-8 rounded-full bg-indigo-700"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                        <span className="sr-only">Your profile</span>
-                        <span aria-hidden="true">Tom Cook</span>
-                      </a>
-                    </li>
+                    {/*<li>*/}
+                    {/*  <div className="text-xs font-semibold leading-6 text-indigo-200">*/}
+                    {/*    Referrals*/}
+                    {/*  </div>*/}
+                    {/*  /!*<ul role="list" className="-mx-2 mt-2 space-y-1">*!/*/}
+                    {/*  /!*  {referralLinks.map((team) => (*!/*/}
+                    {/*  /!*    <li key={team.name}>*!/*/}
+                    {/*  /!*      <a*!/*/}
+                    {/*  /!*        href={team.href}*!/*/}
+                    {/*  /!*        className={classNames(*!/*/}
+                    {/*  /!*          team.current*!/*/}
+                    {/*  /!*            ? "bg-indigo-700 text-white"*!/*/}
+                    {/*  /!*            : "text-indigo-200 hover:text-white hover:bg-indigo-700",*!/*/}
+                    {/*  /!*          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"*!/*/}
+                    {/*  /!*        )}*!/*/}
+                    {/*  /!*      >*!/*/}
+                    {/*  /!*        <span className="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">*!/*/}
+                    {/*  /!*          {team.initial}*!/*/}
+                    {/*  /!*        </span>*!/*/}
+                    {/*  /!*        <span className="truncate">{team.name}</span>*!/*/}
+                    {/*  /!*      </a>*!/*/}
+                    {/*  /!*    </li>*!/*/}
+                    {/*  /!*  ))}*!/*/}
+                    {/*  /!*</ul>*!/*/}
+                    {/*</li>*/}
+                    {/*<li className="-mx-6 mt-auto">*/}
+                    {/*  <a*/}
+                    {/*    href="#"*/}
+                    {/*    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-indigo-700"*/}
+                    {/*  >*/}
+                    {/*    <img*/}
+                    {/*      className="h-8 w-8 rounded-full bg-indigo-700"*/}
+                    {/*      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
+                    {/*      alt=""*/}
+                    {/*    />*/}
+                    {/*    <span className="sr-only">Your profile</span>*/}
+                    {/*    <span aria-hidden="true">Tom Cook</span>*/}
+                    {/*  </a>*/}
+                    {/*</li>*/}
                   </ul>
                 </nav>
               </div>
@@ -261,14 +253,14 @@ const DashboardPage = () => {
                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
               </button>
               <div className="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
-              <a href="#">
-                <span className="sr-only">Your profile</span>
-                <img
-                  className="h-8 w-8 rounded-full bg-indigo-700"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </a>
+              {/*<a href="#">*/}
+              {/*  <span className="sr-only">Your profile</span>*/}
+              {/*  <img*/}
+              {/*    className="h-8 w-8 rounded-full bg-indigo-700"*/}
+              {/*    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"*/}
+              {/*    alt=""*/}
+              {/*  />*/}
+              {/*</a>*/}
             </div>
 
             <main className="py-10 lg:pl-72">
