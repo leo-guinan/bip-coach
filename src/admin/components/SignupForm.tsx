d3d1a31c19c2e7e6708da82726ba1e947bc083c8
leo-guinan <leo@copy.ai>
2023-07-23 18:19:39 -0400

    hiding chat interface if not logged in

--- src/admin/components/SignupForm.tsx
+++ src/admin/components/SignupForm.tsx
@@ -105,7 +105,7 @@ const SignupForm = () => {
                   autoComplete="preferred-name"
                   required
                   className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="Preferred Name"
+                  placeholder="What should we call you?"
                   onChange={(e) => setPreferredName(e.target.value)}
                 />
               </div>
--- src/core/components/Chat.tsx
+++ src/core/components/Chat.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { useMutation } from "@blitzjs/rpc"
 import sendMessage from "../../chat/mutations/sendMessage"
 import ReactMarkdown from "react-markdown"
+import { useCurrentUser } from "../../users/hooks/useCurrentUser"
 
 interface ChatMessage {
   message: string
@@ -10,6 +11,7 @@ interface ChatMessage {
 }
 
 const Chat = () => {
+  const currentUser = useCurrentUser()
   const [message, setMessage] = useState("")
   const [chat, setChat] = useState<ChatMessage[]>([])
 
@@ -34,39 +36,66 @@ const Chat = () => {
 
   return (
     <div className="flex flex-col h-screen bg-gray-800">
-      <div className="overflow-auto w-full h-full p-5 mb-10">
-        {chat.map(({ message, id, source }) => (
-          <>
-            {source === "human" ? (
-              <div key={id} className="my-3 bg-gray-300 rounded p-3 text-sm w-3/4">
-                {message}
-              </div>
-            ) : (
-              <div
-                key={id}
-                className="my-3 bg-gray-700 rounded p-3 text-sm text-white  flex-row-reverse w-3/4"
+      {currentUser && (
+        <>
+          <div className="overflow-auto w-full h-full p-5 mb-10">
+            {chat.map(({ message, id, source }) => (
+              <>
+                {source === "human" ? (
+                  <div key={id} className="my-3 bg-gray-300 rounded p-3 text-sm w-3/4">
+                    {message}
+                  </div>
+                ) : (
+                  <div
+                    key={id}
+                    className="my-3 bg-gray-700 rounded p-3 text-sm text-white  flex-row-reverse w-3/4"
+                  >
+                    <ReactMarkdown>{message}</ReactMarkdown>
+                  </div>
+                )}
+              </>
+            ))}
+          </div>
+
+          <div className="fixed w-full bottom-0">
+            <form onSubmit={handleSendMessage}>
+              <input
+                className="w-full h-8 px-3 rounded mb-0 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 "
+                value={message}
+                placeholder="Type your message here..."
+                onChange={(e) => setMessage(e.target.value)}
+              />
+
+              <button className="hidden" type="submit">
+                Send
+              </button>
+            </form>
+          </div>
+        </>
+      )}
+      {!currentUser && (
+        <>
+          {/*login or signup buttons*/}
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-4xl font-bold">Build In Public Coach</h1>
+            <p className="text-xl">Login or Signup to start chatting</p>
+            <div className="flex gap-4 mt-4">
+              <a
+                href="/login"
+                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
               >
-                <ReactMarkdown>{message}</ReactMarkdown>
-              </div>
-            )}
-          </>
-        ))}
-      </div>
-
-      <div className="fixed w-full bottom-0">
-        <form onSubmit={handleSendMessage}>
-          <input
-            className="w-full h-8 px-3 rounded mb-0 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 "
-            value={message}
-            placeholder="Type your message here..."
-            onChange={(e) => setMessage(e.target.value)}
-          />
-
-          <button className="hidden" type="submit">
-            Send
-          </button>
-        </form>
-      </div>
+                Login
+              </a>
+              <a
+                href="/signup"
+                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Signup
+              </a>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
