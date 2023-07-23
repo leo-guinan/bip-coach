d469ca6718cdbcf2a84990e595e28c1fd7864c70
leo-guinan <leo@copy.ai>
2023-07-23 16:31:43 -0400

    adding name fields to signup form

--- src/admin/components/SignupForm.tsx
+++ src/admin/components/SignupForm.tsx
@@ -24,12 +24,14 @@ const SignupForm = () => {
   const [signupMutation] = useMutation(signup)
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [preferredName, setPreferredName] = useState("")
+  const [fullName, setFullName] = useState("")
   const [errors, setErrors] = useState<string[]>([])
   const router = useRouter()
   const handleSignup = async (e) => {
     e.preventDefault()
     try {
-      await signupMutation({ email, password })
+      await signupMutation({ email, password, fullName, preferredName })
       await router.push("/dashboard")
     } catch (e) {
       const parsedMessage: ZodIssue[] = JSON.parse(e.message)
@@ -74,6 +76,37 @@ const SignupForm = () => {
                   className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="Email address"
                   onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="full-name" className="sr-only">
+                  Full Name
+                </label>
+                <input
+                  id="full-name"
+                  name="full-name"
+                  type="input"
+                  autoComplete="full-name"
+                  required
+                  className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Full Name"
+                  onChange={(e) => setFullName(e.target.value)}
+                />
+              </div>
+
+              <div>
+                <label htmlFor="preferred-name" className="sr-only">
+                  What should we call you?
+                </label>
+                <input
+                  id="preferred-name"
+                  name="preferred-name"
+                  type="input"
+                  autoComplete="preferred-name"
+                  required
+                  className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Preferred Name"
+                  onChange={(e) => setPreferredName(e.target.value)}
                 />
               </div>
               <div>
