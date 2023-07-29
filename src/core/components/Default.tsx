fc778075f516df3d589c0ccd231fe187cc0b4e15
leo-guinan <leo@copy.ai>
2023-07-29 16:07:53 -0400

    handling non-logged in user on index

--- src/core/components/Default.tsx
+++ src/core/components/Default.tsx
@@ -1,5 +1,5 @@
 import { useCurrentUser } from "../../users/hooks/useCurrentUser"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { Routes } from "@blitzjs/next"
 import { useRouter } from "next/router"
 
@@ -17,7 +17,33 @@ const Default = () => {
       )
     }
   }, [router])
-  return <></>
+  return (
+    <>
+      {!currentUser && (
+        <>
+          {/*login or signup buttons*/}
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-4xl font-bold">Build In Public Coach</h1>
+            <p className="text-xl">Login or Signup to start chatting</p>
+            <div className="flex gap-4 mt-4">
+              <a
+                href="/login"
+                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Login
+              </a>
+              <a
+                href="/signup"
+                className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Signup
+              </a>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  )
 }
 
 export default Default
