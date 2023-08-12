702c2e259abad5fe79a2dab502eea97643434bc1
leo-guinan <leo@copy.ai>
2023-08-12 11:29:48 -0400

    forgot to fix mobile

--- src/core/components/ChatsMobile.tsx
+++ src/core/components/ChatsMobile.tsx
@@ -9,21 +9,22 @@ const ChatsMobile = () => {
   const [chats] = useQuery(getChats, null)
   return (
     <ul role="list" className="-mx-2 mt-2 space-y-1">
-      {chats.map((chat) => (
-        <li key={chat.name}>
-          <a
-            href={`${chat.session_id}`}
-            className={classNames(
-              chat.session_id === router.query.session_id
-                ? "bg-gray-800 text-white"
-                : "text-gray-400 hover:text-white hover:bg-gray-800",
-              "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-            )}
-          >
-            <span>{chat.name}</span>
-          </a>
-        </li>
-      ))}
+      {chats &&
+        chats.map((chat) => (
+          <li key={chat.name}>
+            <a
+              href={`${chat.session_id}`}
+              className={classNames(
+                chat.session_id === router.query.session_id
+                  ? "bg-gray-800 text-white"
+                  : "text-gray-400 hover:text-white hover:bg-gray-800",
+                "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+              )}
+            >
+              <span>{chat.name}</span>
+            </a>
+          </li>
+        ))}
     </ul>
   )
 }
